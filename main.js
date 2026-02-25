7454feabc19a6ff1a5bfe016f94623d414b3a21f
HarkishanSingh-22 <kk1721803@gmail.com>
2026-02-25 17:21:20 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const textChange = document.getElementById('text-change');
+
+
+function logBtn() {
+
+    const textChange = document.getElementById('text-change');
+    textChange.innerHTML=('Login with The Souled Store');
+
+}
+function registerBtn() {
+
+    const textChange = document.getElementById('text-change');
+    textChange.innerHTML=('Register with The Souled Store');
+
+}
